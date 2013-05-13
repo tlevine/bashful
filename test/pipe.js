b0d3dc99f8e8581d245d8451fb2375388cbba979
James Halliday <mail@substack.net>
2013-05-13 09:26:49 -0700

    failing pipe test

---
+++ test/pipe.js
@@ -0,0 +1,34 @@
+var test = require('tap').test;
+var bash = require('../');
+var through = require('through');
+var concat = require('concat-stream');
+
+test('run', function (t) {
+    t.plan(1);
+    
+    var sh = bash();
+    sh.on('command', run);
+    
+    var s = sh.createStream();
+    s.pipe(concat(function (err, src) {
+        t.equal(src, [
+            '$ 8',
+            '$ '
+        ].join('\n'));
+    }));
+    s.write('echo beep boop | wc -c\n');
+    s.end();
+});
+
+function run (cmd, args) {
+    if (cmd === 'wc' && args[0] === '-c') {
+        var count = 0;
+        var write = function (buf) { count += buf.length };
+        var end = function () {
+            this.queue(count + '\n');
+            this.queue(null);
+        };
+        var tr = through(write, end);
+        return tr;
+    }
+}
