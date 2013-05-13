c8930a6698e99d511c305b9cf11469010a6b1926
James Halliday <mail@substack.net>
2013-05-13 01:43:22 -0700

    failing test due to timing issues

---
+++ test/run.js
@@ -0,0 +1,40 @@
+var test = require('tap').test;
+var bash = require('../');
+var through = require('through');
+var concat = require('concat-stream');
+
+test('run', function (t) {
+    t.plan(1);
+    
+    var sh = bash({ XYZ: 'abcdefg' });
+    sh.on('command', run);
+    
+    var s = sh.createStream();
+    s.pipe(concat(function (err, src) {
+        t.equal(src, [
+            '$ /home/test',
+            '$ boop',
+            '$ abcdefg',
+            ''
+        ].join('\n'));
+    }));
+    s.write('pwd\n');
+    s.write('beep\n');
+    s.write('echo $XYZ\n');
+    setTimeout(function () { s.end(); }, 50);
+});
+
+function run (cmd, args) {
+    var tr = through();
+    tr.pause();
+    tr.queue({
+        'pwd': '/home/test\n',
+        'beep': 'boop\n',
+        'echo': args.join(' ') + '\n'
+    }[cmd]);
+    
+    process.nextTick(function () {
+        tr.resume();
+    });
+    return tr;
+}
