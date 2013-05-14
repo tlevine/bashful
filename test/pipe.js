1d6681d180c44c32980746e55adadb1871e7dded
James Halliday <mail@substack.net>
2013-05-13 20:27:37 -0700

    pipe test updated

--- test/pipe.js
+++ test/pipe.js
@@ -6,8 +6,7 @@ var concat = require('concat-stream');
 test('run', function (t) {
     t.plan(1);
     
-    var sh = bash();
-    sh.on('command', run);
+    var sh = bash({ command: run });
     
     var s = sh.createStream();
     s.pipe(concat(function (err, src) {
