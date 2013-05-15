b8a9f816603dbcddf929811042a03f9bf5f97b6f
James Halliday <mail@substack.net>
2013-05-15 11:12:52 -0700

    failing multiword test

--- test/env.js
+++ test/env.js
@@ -16,13 +16,31 @@ test('set env vars', function (t) {
     s.end();
 });
 
+test('set quoted env var', function (t) {
+    t.plan(1);
+    
+    var sh = bash({ spawn: run, env: { X: 3 } });
+    
+    var s = sh.createStream();
+    s.pipe(concat(function (err, src) {
+        t.equal(src, '$ 7 8 9\n$ ');
+    }));
+    s.write('X="7 8 9"; printx\n');
+    s.end();
+});
+
 function run (cmd, args, opts) {
+    var tr = through();
+    tr.pause();
+    
     if (cmd === 'printx') {
-        var tr = through();
-        tr.pause();
         tr.queue(opts.env.X + '\n');
-        tr.queue(null);
-        process.nextTick(function () { tr.resume() });
-        return tr;
     }
+    else {
+        tr.queue('No such command: ' + cmd + '\n');
+    }
+    
+    tr.queue(null);
+    process.nextTick(function () { tr.resume() });
+    return tr;
 }
